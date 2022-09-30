07b0f9fc5d2a02903ce5fe19f93c35e25a7aaac2
burrito <77576380+eid7lon@users.noreply.github.com>
2022-09-30 18:07:26 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo
+echo "Installing Base System"
+echo
+
+PKGS=(
+	'librewolf-bin'
+)
+
+for PKG in "${PKGS}"; do
+	echo "installing: ${PKG}"
+	yes | yay -S "$PKG" --answerdiff=None --answerclean=None --needed
+done
+
+echo
+echo "done"
+echo
