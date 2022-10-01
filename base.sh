22fce1ce2ba7717ad8dc0e8482ed5f2821803d6d
burrito <77576380+eid7lon@users.noreply.github.com>
2022-10-01 21:56:12 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,16 +1,30 @@
 echo
-echo "INSTALLING AUDIO COMPONENTS"
+echo "Starting the installation"
 echo
 
 PKGS=(
-            'vlc'
-	          'falkon'
+        'tlp'
+	'wget'
+	#Bluetooth
+	'bluez'
+	'bluez-utils'
+	#Audio
+	'alsa-utils'
+	'alsa-plugins'
+	'pulseaudio'
+	'pulseaudio-alsa'
+	'pulseaudio-bluetooth'
 )
-
 for PKG in "${PKGS[@]}"; do
-    echo "INSTALLING ${PKG}"
+    echo "Installing ${PKG}"
     sudo pacman -S "$PKG" --noconfirm --needed
 done
+
+sudo systemctl enable tlp #Enable laptop power managements
+sudo systemctl mask systemd-rfkill.service #Resolve TLP conflicts
+sudo systemctl mask systemd-rfkill.service #Resolve TLP conflicts
+sudo systemctl enable fstrim.timer #Enable SSD trimmer
+
 
 echo
 echo "Done!"
